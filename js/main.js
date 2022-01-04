fc686387b9aeac2c02d17a4e5353955990382e20
k3vwe <rukkimax@gmail.com>
2022-01-04 11:29:12 +0100

    Introduce JQuery

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    // Pick the navigation and header-body elements
+    $navigation = $('.navigation');
+    $header_body = $('.header-body h1');
+
+    // Select the Hamburger Menu
+    $hamburger = $('.hamburger')
+
+});
